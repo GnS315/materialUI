25c8b8f24233a9dc29386bc1908827182e7eb750
Egor Morozov <e.d.morozov@esoft.local>
2023-11-02 20:34:37 +0500

    materialUi

--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {Button} from "@mui/material";
 
-const MyButton = (props) => {
+const MyButton = ({children, ...otherProps}) => {
   return <Button
     variant="contained"
     sx = {{margin: 1}}
     color = "info"
-    {...props}
-  >{props.children}</Button>
+    {...otherProps}
+  >{children}</Button>
 };
 
 export default MyButton;
